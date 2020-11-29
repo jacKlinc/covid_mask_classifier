decffc259ffdff25c4777140433aa81ba41fe267
jacKlinc <jackharding321@gmail.com>
2020-11-29 15:08:37 +0100

    Streamlit app working

---
+++ src/src_bear-class.py
@@ -0,0 +1,41 @@
+import fastbook
+fastbook.setup_book()
+from fastbook import *
+from fastai.vision.widgets import *
+
+import streamlit as st
+from PIL import Image
+
+# @st.cache
+def load_image(image):
+    return Image.open(image)
+
+def predict_img(img):
+    if img is not None:
+        return learner_inf.predict(pil_img)
+
+'Import the pickle file'
+learner_inf = load_learner('../export.pkl')
+
+# Upload
+pic = st.file_uploader("Upload Files")
+
+probs = []
+pred_idx = 1
+pred = 'n/a'
+
+# Display image
+if pic is not None:
+    img = load_image(pic)
+    st.image(img)
+    
+    # Parse image
+    pil_img = PILImage.create(pic)
+
+    # Predict category
+    pred,pred_idx,probs = predict_img(pil_img)
+
+# Classify
+if st.button('Classify'):
+    'Prediction: ', pred 
+    'Probability: ', probs[pred_idx].item()
